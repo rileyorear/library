8d56bcbe8ce4b7eebf9aa910db2351f8d61e238b
rileyorear <113939466+rileyorear@users.noreply.github.com>
2023-09-25 18:10:37 -0700

    Update book add to page same title bug

--- script.js
+++ script.js
@@ -19,9 +19,36 @@ function addBooktoLibrary(title, author, pages, read) {
   library.push(book);
 }
 
+function addBooktoPage() {
+  const bookElement = document.createElement("div");
+  document.querySelector(".books").appendChild(bookElement);
+  bookElement.classList.add("book");
+  
+  if (document.getElementById(titleInput.value) === null) {
+    bookElement.id = titleInput.value+authorInput.value;
+  
+    const titleElement = document.createElement("p");
+    document.getElementById(bookElement.id).appendChild(titleElement);
+    titleElement.innerHTML += `<strong>Title: </strong>${titleInput.value}`;
+    
+    const authorElement = document.createElement("p");
+    document.getElementById(bookElement.id).appendChild(authorElement);
+    authorElement.innerHTML += `<strong>Author: </strong>${authorInput.value}`;
+    
+    const pagesElement = document.createElement("p");
+    document.getElementById(bookElement.id).appendChild(pagesElement);
+    pagesElement.innerHTML += `<strong>Pages: </strong>${pagesInput.value}`;
+    
+    const readElement = document.createElement("p");
+    document.getElementById(bookElement.id).appendChild(readElement);
+    readElement.innerHTML += `<strong>Read: </strong>${readInput.value}`;
+  }
+}
+
 submit.addEventListener("click", (e) => {
   e.preventDefault();
   addBooktoLibrary(titleInput.value, authorInput.value, pagesInput.value, readInput.value);
+  addBooktoPage();
   form.reset();
   console.log(library);
 });
